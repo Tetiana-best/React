9bf7b6100472762c4707e6c2e8f89b59839f2fc4
Tetiana-best <tanya_gergel1984@ukr.net>
2025-06-19 18:40:26 +0200

    Delete src directory

--- src/components/SalaryList/SalaryItems.js
+++
@@ -1,9 +0,0 @@
-const salaryItems = [
-	{ id: 1, name: "Anna", salary: 3200 },
-	{ id: 2, name: "Boris", salary: 2800 },
-	{ id: 3, name: "Clara", salary: 3500 },
-	{ id: 4, name: "David", salary: 4000 },
-	{ id: 5, name: "Elena", salary: 3100 }
- ]
-
- export default salaryItems
