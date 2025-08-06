b64d6cd20bbce27488030946d4fc07f9dbfd3aae
Tetiana-best <tanya_gergel1984@ukr.net>
2025-08-07 01:46:39 +0200

    Update apiRoutes.js

--- react_hw_08_frontend/src/api/apiRoutes.js
+++ react_hw_08_frontend/src/api/apiRoutes.js
@@ -1,20 +1,20 @@
-// routes.js
-// const API_BASE_URL = 'http://localhost:5000/api'
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const FULL_API_URL = API_BASE_URL + '/api';
+
 
 export default {
   // GET: Отримати всіх вчителів
-  getAllTeachers: `${API_BASE_URL}/teachers`,
+  getAllTeachers: `${FULL_API_URL}/teachers`,
 
   // POST: Створити нового вчителя
-  addTeacher: `${API_BASE_URL}/teachers`,
+  addTeacher: `${FULL_API_URL}/teachers`,
 
   // GET: Отримати вчителя за ID
-  getTeacherById: (id) => `${API_BASE_URL}/teachers/${id}`,
+  getTeacherById: (id) => `${FULL_API_URL}/teachers/${id}`,
 
   // PUT: Оновити вчителя за ID
-  updateTeacher: (id) => `${API_BASE_URL}/teachers/${id}`,
+  updateTeacher: (id) => `${FULL_API_URL}/teachers/${id}`,
 
   // DELETE: Видалити вчителя за ID
-  deleteTeacher: (id) => `${API_BASE_URL}/teachers/${id}`,
+  deleteTeacher: (id) => `${FULL_API_URL}/teachers/${id}`,
 }
