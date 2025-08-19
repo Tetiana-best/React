c71c478351cc9f70e5e9a1308bf3f352568fd34e
Tetiana-best <tanya_gergel1984@ukr.net>
2025-08-19 11:02:20 +0200

    add react_hw_10

--- react_hw_08_frontend/src/api/apiRoutes.js
+++ react_hw_08_frontend/src/api/apiRoutes.js
@@ -1,6 +1,3 @@
-// const API_BASE_URL = 'http://localhost:5000/api'
-// const API_BASE_URL = import.meta.env.VITE_API_URL + '/api';
-
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 const FULL_API_URL = API_BASE_URL + '/api';
 
